86603e4ac8f885f7fc3adc64f6da366833c866a0
Sharon Zhou <sharon.waxseal@gmail.com>
2019-10-21 12:17:47 -0700

    binarized realism score

--- nvidia_compare.py
+++ nvidia_compare.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pathlib import Path
 from scipy import stats
 import json
+import sklearn.metrics as skm
 
 from constants import *
 
@@ -11,8 +12,8 @@ dense_test_csv = IMAGES_DIR / 'dense_test.csv'
 df_nvidia = pd.read_csv(nvidia_csv)
 df_dense = pd.read_csv(dense_test_csv)
 
-#df_nvidia['realism_score_normalized'] = df_nvidia.apply(normalize[df_nvidia['realism_score'] > 0.5]
-df_dense['labels_binarized'] = df_dense[df_dense['labels'] > 0.5]
+df_nvidia['realism_score_binarized'] = df_nvidia['realism_score'] >= 1.0
+df_dense['labels_binarized'] = df_dense['labels'] > 0.5
 
 # Get intersection of filename col
 df = pd.merge(df_nvidia, df_dense, how='inner', on=['filename'])
@@ -23,7 +24,8 @@ metrics = { model: {} for model in models}
 for model in models:
     metrics[model]['pearsonr'], metrics[model]['pearsonr_pval'] = stats.pearsonr(df.loc[df['model']==model, 'labels'], df.loc[df['model']==model, 'realism_score'])
     metrics[model]['spearmanr'], metrics[model]['spearmanr_pval'] = stats.spearmanr(df.loc[df['model']==model, 'labels'], df.loc[df['model']==model, 'realism_score'])
-    metrics[model]['auprc'] = skm.average_precision_score(df.loc[df['model']==model, 'labels_binarized'], df.loc[df['model']==model, 'realism_score_normalized'])
+    metrics[model]['auprc'] = skm.average_precision_score(df.loc[df['model']==model, 'labels_binarized'], df.loc[df['model']==model, 'realism_score_binarized'])
+    metrics[model]['accuracy'] = skm.accuracy_score(df.loc[df['model']==model, 'labels_binarized'], df.loc[df['model']==model, 'realism_score_binarized'])
 print(metrics)
 print(json.dumps(metrics, sort_keys=True, indent=4))
 import pdb;pdb.set_trace()
